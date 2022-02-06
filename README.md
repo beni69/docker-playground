ce3d5d50a574b35907a7ae03dbfba520dc878ebd
beni69 <73666440+beni69@users.noreply.github.com>
2022-02-06 15:29:51 +0100

    initial commit :tada:

---
+++ README.md
@@ -0,0 +1,7 @@
+# rust-docker
+
+just some experiments using docker with rust
+
+the example rust app from
+[the rust website](https://www.rust-lang.org/learn/get-started) along with some
+dockerfiles and scripts
---
+++ build-arm64.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# cross-compile for arm64 and put it in a distroless image
+# note: the compilation is done on the host machine, not in a container
+# this may lead to better performance and less disk space usage,
+# but requires a working rust toolchain and the `cross` tool
+
+set -e
+
+APP_NAME="hello_rust"
+
+# build the executable and place it in the target directory
+cross build --release --target aarch64-unknown-linux-gnu
+
+# create a docker container with the executable
+docker buildx build --load --platform linux/arm64 -t "$APP_NAME:latest" -f Dockerfile.arm64 .
+
+# allow copying the image if the script is ran with a user@host argument
+if [ ! -z $1 ]; then
+    echo "attempting to copy to $1 via ssh"
+
+    # load the resulting image onto the target machine
+    docker save hello_rust | pv | ssh "$1" docker load
+fi
