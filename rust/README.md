6155a5431e493cd8c2af75908922cf96932f965b
beni69 <73666440+beni69@users.noreply.github.com>
2022-03-20 21:42:10 +0100

    cache layer optimize all rust builds

--- rust/README.md
+++ rust/README.md
@@ -5,3 +5,7 @@ just some experiments using docker with rust
 the example rust app from
 [the rust website](https://www.rust-lang.org/learn/get-started) along with some
 dockerfiles and scripts
+
+## image sizes
+- [Rust slim image](Dockerfile.distroless): 806MB
+- [Distroless cc image](Dockerfile.distroless): 26MB (+ 806MB builder)
--- rust/build-arm64.sh
+++ rust/build-arm64.sh
@@ -2,10 +2,14 @@
 # cross-compile for arm64 and put it in a distroless image
 # note: the compilation is done on the host machine, not in a container
 # this may lead to better performance and less disk space usage,
+# and there's no need for docker cache layer magic to have incremental compilation,
 # but requires a working rust toolchain and the `cross` tool
 
+# exit on a falied command
 set -e
 
+#! replace this with the name of the rust project
+#! also replace APP_NAME in `Dockerfile.arm64`
 APP_NAME="hello_rust"
 
 # build the executable and place it in the target directory
